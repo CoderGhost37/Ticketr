d509f85f8d18f423f8567c07e9135322a3f6ec76
CoderGhost37 <coderghost37@gmail.com>
2024-11-29 23:32:17 +0530

    feat: convex setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from "next"
 import localFont from "next/font/local"
+
 import "./globals.css"
-import { ClerkProvider } from "@clerk/nextjs"
+
+import { ConvexClientProvider } from "@/components/ConvexClientProvider"
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -26,11 +29,13 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<ClerkProvider>
-				<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-					{children}
-				</body>
-			</ClerkProvider>
+			<ConvexClientProvider>
+				<ClerkProvider>
+					<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+						{children}
+					</body>
+				</ClerkProvider>
+			</ConvexClientProvider>
 		</html>
 	)
 }
