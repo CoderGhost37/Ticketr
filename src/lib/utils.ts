5f5a236998360746f95d849cb612f081ff0e611b
CoderGhost37 <coderghost37@gmail.com>
2024-11-30 12:09:40 +0530

    feat: file storage setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
 import { type ClassValue, clsx } from "clsx"
+import { useQuery } from "convex/react"
 import { twMerge } from "tailwind-merge"
+import { api } from "../../convex/_generated/api"
+import type { Id } from "../../convex/_generated/dataModel"
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+export function useStorageUrl(storageId: Id<"_storage"> | undefined) {
+	return useQuery(api.storage.getUrl, storageId ? { storageId } : "skip")
 }
