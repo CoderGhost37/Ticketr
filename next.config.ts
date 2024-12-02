bfd5865a26ed601345742eece084b17d0ad7fafb
CoderGhost37 <coderghost37@gmail.com>
2024-12-02 11:48:50 +0530

    feat: create event workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	images: {
+		domains: ["colorless-buzzard-523.convex.cloud"],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
